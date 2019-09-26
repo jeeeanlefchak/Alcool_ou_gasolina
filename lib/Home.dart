eec3cd758e33f91e814351636d1094fb25430d8a
jeeeanlefchak <jean_2103@hotmail.com>
2019-09-26 13:18:31 -0300

    ..

--- lib/Home.dart
+++ lib/Home.dart
@@ -6,9 +6,32 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-
   TextEditingController _controllerAlcool = TextEditingController();
   TextEditingController _controllerGasolina = TextEditingController();
+  String _textoResultado = "";
+
+  void _calcular() {
+    double precoAlcool = double.tryParse(_controllerAlcool.text);
+    double precoGasolina = double.tryParse(_controllerGasolina.text);
+    if (precoAlcool == null || precoGasolina == null) {
+      setState(() {
+        _textoResultado =
+            "Número inválido, digite numeros maiores que 0 e utilizando (.)";
+      });
+    } else {
+      print((precoAlcool / precoGasolina)
+      );
+      if ((precoAlcool / precoGasolina) >= 0.7) {
+        setState(() {
+          _textoResultado = "É melhor abastecer com Gasolina";
+        });
+      } else {
+        setState(() {
+          _textoResultado = "É melhor abastecer com Alcool";
+        });
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -31,30 +54,21 @@ class _HomeState extends State<Home> {
                 padding: EdgeInsets.only(bottom: 10),
                 child: Text(
                   "Saiba qual a melhor opção para abastecimento do seu carro",
-                  style: TextStyle(
-                      fontSize: 25,
-                      fontWeight: FontWeight.bold
-                  ),
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
                 ),
               ),
               TextField(
                 keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                    labelText: "Preço Alcool, ex: 1.59"
-                ),
-                style: TextStyle(
-                    fontSize: 22
-                ),
+                decoration:
+                    InputDecoration(labelText: "Preço Alcool, ex: 1.59"),
+                style: TextStyle(fontSize: 22),
                 controller: _controllerAlcool,
               ),
               TextField(
                 keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                    labelText: "Preço Gasolina, ex: 3.59"
-                ),
-                style: TextStyle(
-                    fontSize: 22
-                ),
+                decoration:
+                    InputDecoration(labelText: "Preço Gasolina, ex: 3.59"),
+                style: TextStyle(fontSize: 22),
                 controller: _controllerGasolina,
               ),
               Padding(
@@ -65,21 +79,15 @@ class _HomeState extends State<Home> {
                     padding: EdgeInsets.all(15),
                     child: Text(
                       "Calcular",
-                      style: TextStyle(
-                          fontSize: 20
-                      ),
+                      style: TextStyle(fontSize: 20),
                     ),
-                    onPressed: (){}
-                ),
+                    onPressed: _calcular),
               ),
               Padding(
                 padding: EdgeInsets.only(top: 20),
                 child: Text(
-                  "Resultado",
-                  style: TextStyle(
-                      fontSize: 22,
-                      fontWeight: FontWeight.bold
-                  ),
+                  _textoResultado,
+                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
                 ),
               )
             ],
